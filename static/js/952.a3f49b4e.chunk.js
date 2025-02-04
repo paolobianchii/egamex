934e61fbb491347f15ab1dc8c5bc41d50b8c59a2
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-04 22:30:34 +0100

    Updates

--- static/js/952.a3f49b4e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkegamesx=self.webpackChunkegamesx||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.a3f49b4e.chunk.js.map
