a6e8e0a43409e4195f5814299c37b0b7184d1806
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-07 23:33:31 +0100

    edit vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://paolobianchii.github.io/egamex/', // Aggiungi il nome del repository GitHub qui
+  base: '/egamex/', // Aggiungi il nome del repository GitHub qui
   build: {
     outDir: 'dist', // Cartella di build
   },
