6979cf14f171ab1b040e11b06e13948c1ce3a676
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-07 23:19:47 +0100

    delete dotenv

--- backend/lib/supabase.js
+++ backend/lib/supabase.js
@@ -1,5 +1,4 @@
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
