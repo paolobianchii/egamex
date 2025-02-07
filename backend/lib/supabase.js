dac5d4b444c264918cf3946f277b299fa0f6a1dd
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-07 23:22:23 +0100

    edit supabase.js

--- backend/lib/supabase.js
+++ backend/lib/supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const SUPABASE_URL = "https://kwxxejdmvgvsteairgyp.supabase.co";
+const SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3eHhlamRtdmd2c3RlYWlyZ3lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3ODcxNDUsImV4cCI6MjA1NDM2MzE0NX0.gdVxilxmw0SwNl-6A9EemvtjSFDd8X0Mzf2K1xJVFXg";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
