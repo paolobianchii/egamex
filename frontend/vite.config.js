c126dceb184c1e37c16674ba589a179937fa7184
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-10 18:14:15 +0100

    change vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/egamex/',  // Usa percorsi relativi per evitare problemi su Netlify
+  base: './',  // Usa percorsi relativi per evitare problemi su Netlify
   build: {
     outDir: 'dist',  // Assicura che Vite generi i file dentro dist/
   },
