fd7d395414546c55c453d8baf2c7001ec3e035fc
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-10 18:17:44 +0100

    edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Layout } from 'antd';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Usa Routes invece di Switch
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Usa Routes invece di Switch
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import Tornei from './components/Tornei';
@@ -13,13 +13,13 @@ const App = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Router basename='/egamex'>
+    <Router>
       <Layout style={{ minHeight: '100vh' }}>
       <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
         <Layout style={{ marginLeft: collapsed ? '80px' : '250px' }}>
           <Content style={{ padding: '0px' }}>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route exact path="/" element={<Home />} />
               <Route path="/tornei" element={<Tornei />} />
               <Route path="/store" element={<Store />} />
               <Route path="/login" element={<Login />} /> {/* Aggiungi la route per la pagina di login */}
