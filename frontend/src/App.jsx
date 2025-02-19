45754a2dd3288a0feb7aa624fa5bee2391580ce0
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-02-19 13:10:02 +0100

    fix redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Layout } from 'antd';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Usa Routes invece di Switch
-import Sidebar from './components/Sidebar';
-import Home from './components/Home';
-import Tornei from './components/Tornei';
-import Store from './components/Store';
-import Login from './components/Login'; 
-import Navbar from './components/Navbar';
-import ModificaProfiloPage from './pages/ModificaProfiloPage';
-import ModificaProfilo from './pages/ModificaProfilo';
-import AdminDashboard from './components/AdminDashboard';
-import PrivateRoute from './pages/PrivateRoute';
+import React, { useState } from "react";
+import { Layout } from "antd";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Usa Routes invece di Switch
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import Tornei from "./components/Tornei";
+import Store from "./components/Store";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import ModificaProfiloPage from "./pages/ModificaProfiloPage";
+import ModificaProfilo from "./pages/ModificaProfilo";
+import AdminDashboard from "./components/AdminDashboard";
+import PrivateRoute from "./pages/PrivateRoute";
 
 const { Content } = Layout;
 
@@ -19,31 +19,33 @@ const App = () => {
 
   return (
     <>
-    <Router>
-      <Layout style={{ minHeight: '100vh' }}>
-      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-      <Navbar/>
-        <Layout style={{ marginLeft: collapsed ? '80px' : '250px' }}>
-          <Content >
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/tornei" element={<Tornei />} />
-              <Route path="/store" element={<Store />} />
-              <Route path="/edit" element={<ModificaProfilo />}/>
-              <Route 
-          path="/adminDashboard" 
-          element={
-            <PrivateRoute 
-              element={<AdminDashboard />} 
-              roleRequired="admin" 
-            />
-          } 
-        />              <Route path="/login" element={<Login />} /> {/* Aggiungi la route per la pagina di login */}
-            </Routes>
-          </Content>
+      <Router>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+          <Navbar />
+          <Layout style={{ marginLeft: collapsed ? "80px" : "250px" }}>
+            <Content>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/tornei" element={<Tornei />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/edit" element={<ModificaProfilo />} />
+                <Route
+                  path="/adminDashboard"
+                  element={
+                    <PrivateRoute
+                      element={<AdminDashboard />}
+                      roleRequired="admin"
+                    />
+                  }
+                />
+                <Route path="/login" element={<Login />} />{" "}
+                {/* Aggiungi la route per la pagina di login */}
+              </Routes>
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
-    </Router>
+      </Router>
     </>
   );
 };
--- frontend/src/pages/PrivateRoute.jsx
+++ frontend/src/pages/PrivateRoute.jsx
@@ -1,22 +1,18 @@
 import { Route, Navigate } from 'react-router-dom';
 
-// Componente PrivateRoute per proteggere le rotte
-const PrivateRoute = ({ element, roleRequired, ...rest }) => {
+const PrivateRoute = ({ element, roleRequired }) => {
   const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role'); // Ruolo dell'utente salvato nel localStorage
+  const role = localStorage.getItem('role'); 
 
-  // Se l'utente non è loggato, reindirizza alla pagina di login
   if (!token) {
     return <Navigate to="/" replace />;
   }
 
-  // Se il ruolo non corrisponde a quello richiesto, reindirizza alla home
   if (role !== roleRequired) {
     return <Navigate to="/" replace />;
   }
 
-  // Se l'utente è loggato e ha il ruolo corretto, mostra l'elemento
-  return element;
+  return element; // Rende l'elemento passata come prop
 };
 
 export default PrivateRoute;
