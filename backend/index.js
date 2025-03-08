9b4b76d9653f7e51563edba05a5c58b5fbdbce0c
Paolo Bianchi <82119885+paolobianchii@users.noreply.github.com>
2025-03-08 01:18:59 +0100

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -32,12 +32,8 @@ app.use(xss()); // Pulisce input dannosi da XSS
 
 app.use(bodyParser.json());
 // Modifica la configurazione CORS all'inizio del file
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 passport.use(
     new DiscordStrategy(
